fb8144bb8c4b1edc92f69e6a8104d85e03de32ed
deepakcoder1 <deepakkumarrasya@gmail.com>
2023-11-14 14:11:46 +0530

    first part is done

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-console.log("hello world");
-console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+
+
+
+function backgroundchange() {
+    const colors = ["red", "blue", "orange", "aqua", "pink", "purple", "brown", "grey", "yellow"]
+    const childrens = Array.from(document.getElementById("parent").children);
+
+    childrens.forEach((element) => {
+        element.style.backgroundColor = colors[Math.floor(Math.random() * 9)]
+
+    });
+
+}
+
+document.getElementById('btn').addEventListener("click", backgroundchange)
